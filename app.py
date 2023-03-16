bc830d833fbcf1008ea580e39cae2bb3d7160bd7
Darwesh Fazil <darwesh.fazil@superops.ai>
2023-03-16 16:53:33 +0530

    updated app

--- app.py
+++ app.py
@@ -1,18 +1,62 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, session
+from werkzeug.utils import secure_filename
+import os
+import predict as p
 
 app = Flask(__name__)
+
+# Defining upload folder path
+UPLOAD_FOLDER = os.path.join('static', 'uploads')
+# # Define allowed files
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+generatedCaption = ''
+
+# Configure upload folder for Flask application
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Define secret key to enable session
+app.secret_key = 'Random12$@'
 
 @app.route('/')
 def hello():
     return render_template('index.html')
 
-@app.route('/getGreetings')
-def getGreetings():
-    return render_template('greetings.html')
+@app.route('/uploadImage')
+def index():
+    return render_template('image.html')
+ 
+@app.route('/uploadImage',  methods=["POST", "GET"])
+def uploadFile():
+    if request.method == 'POST':
+        # Upload file flask
+        uploaded_img = request.files['uploaded-file']
+        session['uploaded_image_name'] = uploaded_img.filename
+        # Extracting uploaded data file name
+        img_filename = secure_filename(uploaded_img.filename)
+        # Upload file to database (defined uploaded folder in static path)
+        uploaded_img.save(os.path.join(app.config['UPLOAD_FOLDER'], img_filename))
+        # Storing uploaded file path in flask session
+        session['uploaded_img_file_path'] = os.path.join(app.config['UPLOAD_FOLDER'], img_filename)
+ 
+        return render_template('image2.html')
+ 
+@app.route('/generateCaption')
+def displayImage():
+    # Retrieving uploaded file path from session
+    img_file_path = session.get('uploaded_img_file_path', None)
+    image_name = session.get('uploaded_image_name')
+    generatedCaption = p.generate_caption(image_name)
+    # Display image in Flask application web page
+    return render_template('show.html', user_image = img_file_path, caption = generatedCaption)
+
+@app.route('/generateCaption')
+def generateCaption():
+    request.get_data
 
 def initialize():
     print("<-----This function will run once----->")
 
 if __name__ == '__main__' :
     initialize()
-    app.run(debug=False)
+    app.run(debug=False, host='0.0.0.0')
---
+++ predict.py
@@ -0,0 +1,97 @@
+import pickle
+import os
+import numpy as np
+
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.applications.vgg16 import preprocess_input
+
+PATH_TO_BASE_DIRECTORY = 'utils'
+PATH_TO_IMAGE_DIRECTORY = 'static/uploads'
+
+def init_all_captions():
+    # load and return all_captions
+    return pickle.load(open(os.path.join(PATH_TO_BASE_DIRECTORY, 'all_captions.pkl'), 'rb'))
+
+def init_feature_model():
+    # load and return feature_model
+    return pickle.load(open(os.path.join(PATH_TO_BASE_DIRECTORY, 'feature_model.pkl'), 'rb'))
+
+def init_model():
+    # load and return the model
+    return pickle.load(open(os.path.join(PATH_TO_BASE_DIRECTORY, 'trained_model_30k_15e_32b'), 'rb'))
+
+def tokenize_text(all_captions):
+    # tokenize the text
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(all_captions)
+    return tokenizer
+
+def get_vocab_size(tokenizer):
+    # return the vocab_size
+    return len(tokenizer.word_index) + 1
+
+def get_max_length(all_captions):
+    # get maximum length of the caption available
+    return max(len(caption.split()) for caption in all_captions)
+
+def load_features(img_name, feature_model):
+    # load the image from file
+    img_path = PATH_TO_IMAGE_DIRECTORY + '/' + img_name
+    image = load_img(img_path, target_size=(224, 224))
+    # convert image pixels to numpy array
+    image = img_to_array(image)
+    # reshape data for model
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    # preprocess image for vgg
+    image = preprocess_input(image)
+    # extract features
+    return feature_model.predict(image, verbose=0)
+
+def idx_to_word(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == integer:
+            return word
+    return None
+
+# generate caption for an image
+def predict_caption(model, image, tokenizer, max_length):
+    # add start tag for generation process
+    in_text = 'startseq'
+    # iterate over the max length of sequence
+    for i in range(max_length):
+        # encode input sequence
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        # pad the sequence
+        sequence = pad_sequences([sequence], max_length)
+        # predict next word
+        yhat = model.predict([image, sequence], verbose=0)
+        # get index with high probability
+        yhat = np.argmax(yhat)
+        # convert index to word
+        word = idx_to_word(yhat, tokenizer)
+        # stop if word not found
+        if word is None:
+            break
+        # append word as input for generating next word
+        in_text += " " + word
+        # stop if we reach end tag
+        if word == 'endseq':
+            break
+      
+    return in_text
+
+def generate_caption(image_name):
+    # load the image
+    feature_model = init_feature_model()
+    feature = load_features(image_name, feature_model)
+
+    # initialize model
+    all_captions = init_all_captions()
+    model = init_model()
+    tokenizer = tokenize_text(all_captions)
+    max_length = get_max_length(all_captions)
+
+    # predict and return the caption
+    return predict_caption(model, feature, tokenizer, max_length).replace('startseq ', '').replace(' endseq', '')
